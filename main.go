d84adeca63f1d32220a339f418bcea8b7efd4809
Nahshon Unna-Tsameret <nahsh.ut@gmail.com>
2023-05-30 10:46:55 +0300

    remove commented code
    also, fix error handling

--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 	"os"
 )
 
-//var fileName string
-//
-//func init() {
-//	flag.StringVar(&fileName, "input", "", "input file path; if missing, reading from stdin")
-//	flag.Parse()
-//}
-
 type User struct {
 	Name  string `json:"name,omitempty"`
 	Email string `json:"email,omitempty"`
@@ -22,18 +15,13 @@ type User struct {
 func main() {
 	if len(os.Args) > 1 {
 		for _, fileName := range os.Args[1:] {
-			readFromFile(fileName)
+			if err := readFromFile(fileName); err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
 		}
 		return
 	}
-
-	//if len(fileName) > 0 {
-	//	reader, err = os.Open(fileName)
-	//	if err != nil {
-	//		fmt.Fprintf(os.Stderr, "can't open file %s; %v\n", fileName, err)
-	//		os.Exit(1)
-	//	}
-	//}
 
 	var (
 		reader io.Reader = os.Stdin
@@ -47,18 +35,19 @@ func main() {
 	}
 }
 
-func readFromFile(fileName string) {
+func readFromFile(fileName string) error {
 	reader, err := os.Open(fileName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "can't open file %s; %v\n", fileName, err)
-		os.Exit(1)
+		return fmt.Errorf("can't open file %s; %w", fileName, err)
 	}
 	defer reader.Close()
+
 	err = read(reader)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "faile to get data from file %s; %v\n", fileName, err)
-		os.Exit(1)
+		return fmt.Errorf("faile to get data from file %s; %w", fileName, err)
 	}
+
+	return nil
 }
 
 func read(reader io.Reader) error {
